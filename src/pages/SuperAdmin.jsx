c085f2a9f9a834a564b9e17cc2c0f9b969c77cba
piragazh <piragazh@gmail.com>
2025-12-28 00:13:51 +0000

    File changes

--- src/pages/SuperAdmin.jsx
+++ src/pages/SuperAdmin.jsx
@@ -3,6 +3,7 @@ import { base44 } from '@/api/base44Client';
 import { useQuery } from '@tanstack/react-query';
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import SystemOverview from '@/components/superadmin/SystemOverview';
 import RestaurantManagement from '@/components/superadmin/RestaurantManagement';
 import MessagingCenter from '@/components/superadmin/MessagingCenter';
